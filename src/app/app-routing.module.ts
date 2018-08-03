6a8d402a41be9067f5ee66930b308c85f8cdba1e
Oscardo <Oscardo.ortiz@gmail.com>
2018-08-03 16:49:41 -0500

    Oscar Eduardo Ortiz Pinzón

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const app_routes: Routes = [
+    { path: 'home', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot( app_routes, { useHash: true } )
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AppRouting, AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
